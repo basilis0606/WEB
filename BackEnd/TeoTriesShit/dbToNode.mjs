9c679414dd04bd1a8aa914e81d8d03a7b71b1be4
Sfikas <sfikas.teo@gmail.com>
2023-04-02 12:42:43 +0300

    Update dbToNode.mjs
    Signed-off-by: Sfikas <sfikas.teo@gmail.com>

--- BackEnd/TeoTriesShit/dbToNode.mjs
+++ BackEnd/TeoTriesShit/dbToNode.mjs
@@ -2,30 +2,63 @@
 import { Mariadb } from "./mariadb.mjs";
 const mariadb = await Mariadb.createConnection();
 
-export async function storesToGeoJson(){
-	let stores = await mariadb.query("select * from stores");
-	// Initialize the geojson features
-	// Using the data from the stores
-	// return call
-	const stores_geojson = {
+function jsonToGeoJson(input,format){
+	const geojson = {
 		type: "FeatureCollection",
 		features: [],
 	};
-	
-	for(let i=0; i<stores.length; i++){
+	for(let i=0; i<input.length; i++){
+		let featureProperties = [];
+		for (let j=0; format.properties.length; j++){
+
+		}
 		stores_geojson.features.push({
 			"type": "Feature",
 			"geometry": {
 			  "type": "Point",
-			  "coordinates": [ stores[i].lon, stores[i].lat]
+			  "coordinates": [ input[i].format[longitude], input[i].format[latitude]]
 			},
 			"properties": {
-			  "id": stores[i].id,
-			  "sale_exists": stores[i].sale_exists,
-			  "name": stores[i].name
+			  "id": input[i].id,
+			  "sale_exists": input[i].sale_exists,
+			  "name": input[i].name
 			}
 		});
 	}
+	return geojson;
+}
+
+function storesGeoJsonFormat(input){
+	let storeFormat = {
+		"type": "Feature",
+		"geometry": {
+		  "type": "Point",
+		  "coordinates": [ input.lon, input.lat ]
+		},
+		"properties": {
+		  "id": input.id,
+		  "sale_exists": input.sale_exists,
+		  "name": input.name
+		}
+	}
+	return storeFormat;
+}
+
+function arrayjsonFormatter(input,format){
+	let output = []
+	for ( let i=0; i<input.length;i++){
+		output.push(format(input[i]));
+	}
+	return output;
+}
+
+export async function storesToGeoJson(){
+	let stores = await mariadb.query("select * from stores");
+	
+	const stores_geojson = {
+		type: "FeatureCollection",
+		features: arrayjsonFormatter(stores,storesGeoJsonFormat)
+	};
 	return stores_geojson;
 }
 
@@ -37,6 +70,20 @@ export async function storeInfo(storeId){
 	return result;
 }
 
+export async function storesFromCategory(categoryid){
+	let query = `select stores.id,stores.name,stores.lon,stores.lat,stores.sale_exists from \
+	categories inner join subcategories on categories.id = subcategories.categories_id inner join \
+	products on subcategories.id = products.subcategories_id inner join sales on products.id = \
+	sales.product_id inner join stores on stores.id = sales.store_id where categories.id = ${categoryid}`
+	let result = await mariadb.query(query);
+
+	const stores_geojson = {
+		type: "FeatureCollection",
+		features: arrayjsonFormatter(result,storesGeoJsonFormat)
+	};
+	return stores_geojson;
+}
+
 export async function tableInfo(tableName, tableFields){
 	let fields = tableFields[0];
 	for (let i=1;i<tableFields.length;i++){
@@ -45,3 +92,7 @@ export async function tableInfo(tableName, tableFields){
 	let table_json = await mariadb.query(`select ${fields} from ${tableName}`);
 	return table_json;
 }
+
+//select stores.id,stores.name,stores.lon,stores.lat,stores.sale_exists from categories inner join subcategories on categories.id = subcategories.categories_id inner join products on subcategories.id = products.subcategories_id inner join sales on products.id = sales.product_id inner join stores on stores.id = sales.store_id where categories.id = 1
+//select * from stores inner join sales on stores.id = sales.store_id
+//select * from sales inner join stores on stores.id = sales.store_id
